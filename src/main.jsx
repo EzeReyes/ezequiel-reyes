67401276321b9b82baf65a21f8479a95ee2c5db5
Ezequiel Reyes <ese.reyes1992@gmail.com>
2025-09-17 15:15:39 -0300

    modificaciones para envio de email

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ import App from './App.jsx'
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 
+// const API_URL = "http://localhost:4000/graphql";
+
 const client = new ApolloClient({
   link: new HttpLink({ uri: import.meta.env.VITE_GRAPHQL_URI }),
   cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   link: new HttpLink({uri: API_URL}),
+//   cache: new InMemoryCache(),
+// });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
